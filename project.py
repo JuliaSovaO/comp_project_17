52ceacce489db880bd24dfaff6fd5441a4d9e2e4
Yuliia Sova <sovaulia7@gmail.com>
2024-11-13 19:23:41 +0200

    Patterns

---
+++ project.py
@@ -0,0 +1,18 @@
+def read_graph(filepath: str) -> tuple[dict, list[list[str]]]:
+    pass
+
+
+def read_items(filepath: str) -> list[dict[str, tuple[str, float]]]:
+    pass
+
+
+def shortest_path(graph: tuple[dict, list], start: dict, end: dict) -> list[tuple]:
+    pass
+
+
+def load_into_truck(items: list[dict[str, tuple]]) -> list[tuple]:
+    pass
+
+
+def get_directions(path: list[tuple]) -> list[str]:
+    pass
